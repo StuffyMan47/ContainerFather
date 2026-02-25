68f3952a18efca5e18a8e6d19a7682648612ef53
marselF <fazleev_01@mail.ru>
2026-02-25 23:24:11 +0300

    fix: поправил кнопки

--- ContainerFather.Bot/Services/BroadcastService.cs
+++ ContainerFather.Bot/Services/BroadcastService.cs
@@ -301,8 +301,10 @@ public class BroadcastService : IBroadcastService
             disableNotification: false,
             replyMarkup: new InlineKeyboardMarkup(new[]
             {
+                new[] { InlineKeyboardButton.WithUrl("Биржа контейнеров в Max", "https://max.ru/join/6V2LpcbRPjbb1a5_sPclvd781XFjG18IIA-bL7NM4PM") },
                 new[] { InlineKeyboardButton.WithUrl("Кэш-сервиc", "https://t.me/cash_servise") },
-                new[] { InlineKeyboardButton.WithUrl("Есть груз/пустой", "https://t.me/+0dWxrmVrUellOTYy") }
+                new[] { InlineKeyboardButton.WithUrl("Есть груз/пустой", "https://t.me/+0dWxrmVrUellOTYy") },
+                new[] { InlineKeyboardButton.WithUrl("Нужен груз/машина в Max", "https://max.ru/join/JCUpKTeABgVlJyTNhF_fkkrmceQ3-NLqTX5tpL4kmdQ") }
             })
         );
     }
@@ -325,6 +327,8 @@ public class BroadcastService : IBroadcastService
             replyMarkup: new InlineKeyboardMarkup(new[]
             {
                 new[] { InlineKeyboardButton.WithUrl("Кэш-сервиc", "https://t.me/cash_servise") },
+                new[] { InlineKeyboardButton.WithUrl("Нужен груз/машина в Max", "https://max.ru/join/JCUpKTeABgVlJyTNhF_fkkrmceQ3-NLqTX5tpL4kmdQ") }
+
             })
         );
     }
